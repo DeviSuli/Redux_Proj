22c685371926c71607f11b883ae6fe3c8c994deb
suli ajibola <suli.ajibola.dev@gmail.com>
2023-02-09 23:58:09 -0500

    minor commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./containers/Header";
 
 function App() {
   return (
     <div className="App">
-      <h2>Help</h2>
+      <Header />
     </div>
   );
 }
