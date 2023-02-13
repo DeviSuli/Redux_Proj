6f74c06be4b76a9a3cb094d6c0bc397b1384d85d
suli ajibola <suli.ajibola.dev@gmail.com>
2023-02-12 23:15:46 -0500

    updated commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./containers/Header";
-
-import ProductDetails from "./containers/ProductDetails";
-import ProductListings from "./containers/ProductListings";
+import Counter from "./features/counter/Counter";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<ProductListings />} />
-          <Route path="/product/:productId" element={<ProductDetails />} />
-          <Route>404 Not Found!</Route>
-        </Routes>
-      </Router>
-    </div>
+    <main className="App">
+      <Counter />
+    </main>
   );
 }
 
